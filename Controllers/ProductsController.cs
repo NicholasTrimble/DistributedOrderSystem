930bda41271f30a429ce64008985e8c7e503d6c7
Nicholas Trimble <nicktrimble07@gmail.com>
2025-12-24 21:59:11 -0600

    finished getting READ setup

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using DistributedOrderSystem.Domain.Models;
+﻿using DistributedOrderSystem.Domain.Models;
+using DistributedOrderSystem.DTOs;
 using DistributedOrderSystem.Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
 
 namespace DistributedOrderSystem.Controllers
 {
@@ -21,6 +23,30 @@ namespace DistributedOrderSystem.Controllers
         {
             var products = await _context.Products.ToListAsync();
             return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] ProductCreateDto productDto)
+        {
+            var product = new Product
+            {
+                Name = productDto.Name,
+                Price = productDto.Price,
+                StockQuantity = productDto.Stock
+            };
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            var productReadDto = new ProductReadDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.StockQuantity,
+            };
+            
+            
+            return CreatedAtAction(nameof(GetProducts), new { id = product.Id }, productReadDto);
         }
     }
 }
--- DTOs/ProductCreateDto.cs
+++ DTOs/ProductCreateDto.cs
@@ -2,5 +2,8 @@
 {
     public class ProductCreateDto
     {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
     }
 }
